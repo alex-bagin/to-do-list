5db45b431b68f7b3b9bcd05b7c876eab00a29449
Alex Bagin <alex.bagin@gmx.de>
2024-02-02 17:10:08 +0100

    #

--- utils/database.js
+++ utils/database.js
@@ -1,12 +1,9 @@
-const Sequelize = require('sequelize');
+const Sequelize = require("sequelize");
+const keys = require("../keys/index");
 
-const DB_NAME = 'node-todo';
-const USER_NAME = 'root';
-const PASSWORD = 'Vs@L5*2!N4@a';
-
-const sequelize = new Sequelize(DB_NAME, USER_NAME, PASSWORD, {
-  host: 'localhost',
-  dialect: 'mysql',
+const sequelize = new Sequelize(keys.DB_NAME, keys.USER_NAME, keys.PASSWORD, {
+  host: "localhost",
+  dialect: "mysql",
 });
 
 module.exports = sequelize;
